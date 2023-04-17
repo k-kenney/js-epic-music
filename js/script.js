9bb63ae951f480004928a68947bf99ba44a385a1
Kayla Kenney <kaylachristinekenney@gmail.com>
2023-04-17 07:41:45 +0200

    used forEach function to append songs to list

--- js/script.js
+++ js/script.js
@@ -18,6 +18,18 @@ const button = document.querySelector(".show-list");
 const total = document.querySelector(".total");
 
 button.addEventListener("click", function () {
+  mixInfo(epicMix);
   mixList.classList.remove("hide");
   button.classList.add("hide");
 });
+
+total.innerText = `${epicMix.length} great songs!`;
+
+const mixInfo = function (mix) {
+  mix.forEach(function (song, index) {
+    let li = document.createElement("li");
+    li.classList.add("song");
+    li.innerHTML = `<span class="song-number">#${index + 1}</span> ${song}`;
+    mixList.append(li);
+  });
+};
